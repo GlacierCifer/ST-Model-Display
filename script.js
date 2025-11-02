4e313bb71eddabfc6e44e56afde41895f399c760
GlacierCifer <112709818+GlacierCifer@users.noreply.github.com>
2025-11-02 13:39:36 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -655,7 +655,7 @@ const PlaceholderModule = {
 const SloganInjectionModule = {
     initialized: false,
     PROMPT_TEXT: [
-        '请在每次回答末尾额外输出一个隐藏的HTML元素，格式为 `<div hidden class="slogan-container">✦❋内容</div>`。',
+        '请在每次正文的末尾额外输出一个隐藏的HTML元素，格式为 `<div hidden class="slogan-container">✦❋内容</div>`。',
         '元素内仅包含当前角色用上帝视角对当下情况的个人感受，最长 15 个汉字。',
         '标语在隐藏元素之外不要重复，也不要额外解释。'
     ].join('\n'),
